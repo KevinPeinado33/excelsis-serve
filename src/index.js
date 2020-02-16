f9274f253471282791837c7131bbfcb6482ad6a0
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2020-02-16 18:54:37 -0500

    cors arreglado

--- src/index.js
+++ src/index.js
@@ -7,10 +7,19 @@ app.set('port', process.env.PORT || 4000);
 //Middlewares
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
 //Routes
 app.use(require('./routes/user'));
 
 //iniciando el servidor
 app.listen(app.get('port'), () => {
-    console.log("Ejecute on port 4000");
+  console.log("Ejecute on port 4000");
 });
