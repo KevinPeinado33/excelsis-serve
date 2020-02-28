7a315bd73126e914bd0fa707704abea1766b351e
Anvorguesa <38229854+KevinPeinado33@users.noreply.github.com>
2020-02-28 13:52:57 -0500

    servicio rest construido para modulo noticias

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const mysqlConnection = require('../../db/database');
 
 router.get('/', (req, res) => {
-    mysqlConnection.query('SELECT * FROM usuario', (error, rows, fields) => {
+    mysqlConnection.query('SELECT * FROM usuario', (error, rows) => {
         if(!error) {
             res.json(rows);
         } else {
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/listado-noticia', (req, res) => {
-    mysqlConnection.query('SELECT * FROM noticia WHERE estado = 1', (error, rows, fields) => {
+    mysqlConnection.query('SELECT * FROM noticia WHERE estado = 1', (error, rows) => {
         if(!error) {
             res.json(rows);
         } else {
@@ -25,7 +25,7 @@ router.get('/listado-noticia', (req, res) => {
 
 router.get('/:idusuario', (req, res) => {
     const { idusuario } = req.params;
-    mysqlConnection.query('SELECT * FROM usuario WHERE idusuario = ?', [idusuario], (error, rows, fields) => {
+    mysqlConnection.query('SELECT * FROM usuario WHERE idusuario = ?', [idusuario], (error, rows) => {
         if(!error) {
             res.json(rows[0]); // manda un objeto
             //res.json(rows[0]); // manda un arreglo con un objeto
@@ -37,9 +37,20 @@ router.get('/:idusuario', (req, res) => {
 
 router.post('/validar-usuario', (req, res) => {
     const { usuario, password } = req.body;
-    mysqlConnection.query('SELECT idusuario, estado, nombres, apellidos FROM usuario WHERE usuario = ? and password = ?', [usuario, password], (error, rows, fields) => {
+    mysqlConnection.query('SELECT idusuario, estado, nombres, apellidos FROM usuario WHERE usuario = ? and password = ?', [usuario, password], (error, rows) => {
         if(!error) {
             res.json(rows[0]);
+        } else {
+            console.log(error);
+        }
+    });
+});
+
+router.post('/registrar-noticias', (req, res) => {
+    const { titulo, categoria, lugar } = req.body;
+    mysqlConnection.query('INSERT INTO noticia(titulo, categoria, lugar, estado) values(?, ?, ?, 1)', [titulo, categoria, lugar], (error) => {
+        if(!error) {
+            res.json('registrado correctamente');
         } else {
             console.log(error);
         }
