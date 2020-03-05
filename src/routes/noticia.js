e5ed945743dba63f74f64bf42b84c501998295a0
Anvorguesa <38229854+KevinPeinado33@users.noreply.github.com>
2020-03-05 01:53:54 -0500

    servidor arreglado

--- src/routes/noticia.js
+++ src/routes/noticia.js
@@ -14,14 +14,25 @@ router.get('/listado-noticia', (req, res) => {
 });
 
 router.post('/registrar-noticias', (req, res) => {
-    const { titulo, categoria, lugar } = req.body;
-    mysqlConnection.query('INSERT INTO noticia(titulo, categoria, lugar, estado) values(?, ?, ?, 1)', [titulo, categoria, lugar], (error) => {
+    const { titulo, categoria, lugar, picture } = req.body;
+    mysqlConnection.query('INSERT INTO noticia(titulo, categoria, lugar, url_imagen,estado) values(?, ?, ?, ?, 1)', [titulo, categoria, lugar, picture], (error) => {
         if(!error) {
             res.json('registrado correctamente');
         } else {
             console.log(error);
         }
     });
+});
+
+router.put('/cambiar-estado-noticia/:idnoticia', (req, res) => {
+    const { idnoticia } = req.params;
+    mysqlConnection.query('UPDATE noticia SET estado = 2 WHERE idnoticia = ?', [idnoticia], (error, rows) => {
+        if(!error) { 
+            res.json('Actualizado Correctamente');
+        } else {
+            console.log(error);
+        }
+    })
 });
 
 module.exports = router;
